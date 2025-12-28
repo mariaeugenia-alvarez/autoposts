bdd7d11227e842ad2b0e718b56f97f68259f0335
Maru <mariaeu.alvarez@gmail.com>
2025-12-28 13:27:12 +0100

    Refactor main function to include Notion API request and response handling

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import os
 from dotenv import load_dotenv
 
+import requests
+
 
 def main():
     # Cargar variables de entorno desde el archivo .env
     load_dotenv()
 
-    # Obtener la variable de entorno
-    message = os.getenv("MESSAGE", "No se encontró la variable MESSAGE")
+    url = "https://api.notion.com/v1/databases/2d6f99825fef8046b59af865011fb0b9/query"
 
-    print(message)
+    notion_api = os.getenv("NOTION_API_KEY")
+
+    headers = {
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + notion_api,
+    }
+    data = {"page_size": 10}
+
+    response = requests.post(url, headers=headers, json=data)
+    # Ver respuesta
+    print(response.status_code)
+    print(response.json())
 
 
 if __name__ == "__main__":
